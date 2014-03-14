7148d8ec9862d136ea48b44e418bed47e6a8a124
Neil Ellis <neil@cazcade.com>
2014-03-14 23:06:10 +0000

    General improvements and fixes

--- providers/do/deprovision.sh
+++ providers/do/deprovision.sh
@@ -4,16 +4,6 @@ if ! which tugboat
 then
     sudo gem install tugboat
 fi
-#Usage:
-  #tugboat create NAME
-#
-#Options:
-  #-s, [--size=N]              # The size_id of the droplet
-  #-i, [--image=N]             # The image_id of the droplet
-  #-r, [--region=N]            # The region_id of the droplet
-  #-k, [--keys=KEYS]           # A comma separated list of SSH key ids to add to the droplet
-  #-p, [--private-networking]  # Enable private networking on the droplet
-
 if tugboat info $1
 then
     tugboat destroy -c $1
--- providers/do/make-image.sh
+++ providers/do/make-image.sh
@@ -1,12 +1,3 @@
-#Usage:
-  #tugboat snapshot SNAPSHOT_NAME FUZZY_NAME [OPTIONS]
-#
-#Options:
-  #-i, [--id=ID]      # The ID of the droplet.
-  #-n, [--name=NAME]  # The exact name of the droplet
-#
-
-
 #!/bin/sh
 trap 'echo FAILED' ERR
 
@@ -15,15 +6,7 @@ if ! which tugboat
 then
     sudo gem install tugboat
 fi
-#Usage:
-  #tugboat create NAME
-#
-#Options:
-  #-s, [--size=N]              # The size_id of the droplet
-  #-i, [--image=N]             # The image_id of the droplet
-  #-r, [--region=N]            # The region_id of the droplet
-  #-k, [--keys=KEYS]           # A comma separated list of SSH key ids to add to the droplet
-  #-p, [--private-networking]  # Enable private networking on the droplet
+
 if tugboat info_image $2
 then
     tugboat destroy_image -c $2

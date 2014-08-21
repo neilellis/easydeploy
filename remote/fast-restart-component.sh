94d99ebdfaf0142dadd3ac156d1249b29c076a42
Neil Ellis <neil@cazcade.com>
2014-08-21 21:31:36 +0100

    Preparing for blue/green

---
+++ remote/fast-restart-component.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -x
+
+. /home/easydeploy/bin/env.sh
+
+
+if ! service supervisor status
+then
+    echo "Supervisor not running, cannot restart component"
+    exit 1
+fi
+
+touch /tmp/.restart-in-progress
+/ezbin/lb_off.sh
+sleep 30
+supervisorctl restart ${COMPONENT}:
+sleep 30
+/ezbin/lb_on.sh
+rm /tmp/.restart-in-progress
+exit 0

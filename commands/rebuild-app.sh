456a291db67dec95fed3968737e92a6329272200
Neil Ellis <neil@cazcade.com>
2014-08-21 23:49:54 +0100

    Preparing for blue/green

--- commands/rebuild-app.sh
+++ commands/rebuild-app.sh
@@ -16,7 +16,7 @@ then
             ssh  -o "StrictHostKeyChecking no" easyadmin@${machine} "sudo /ezbin/fast-restart-component.sh 30"
     done
 else
-   ../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | parallel --gnu -P 0  "set -eux; sync ${DIR}/*  easydeploy@{}:~/project/; ssh  -o 'StrictHostKeyChecking no' ${USERNAME}@{} 'sudo chown -R easydeploy:easydeploy  /home/easydeploy/project';ssh  -o 'StrictHostKeyChecking no' easydeploy@{} 'cd project; docker build .'; ssh  -o 'StrictHostKeyChecking no' easyadmin@{} 'sudo supervisorctl restart all'"
+   ../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | parallel --gnu -P 0  "set -eux; sync ${DIR}/*  easydeploy@{}:~/project/; ssh  -o 'StrictHostKeyChecking no' ${USERNAME}@{} 'sudo chown -R easydeploy:easydeploy  /home/easydeploy/project';ssh  -o 'StrictHostKeyChecking no' easydeploy@{} 'cd project; docker build .'; ssh  -o 'StrictHostKeyChecking no' easyadmin@{} 'sudo /ezbin/fast-restart-component.sh 30'"
 fi
 
 

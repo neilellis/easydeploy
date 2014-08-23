3e9bf097cfd82dcf432277ac915e2273554601b5
Neil Ellis <neil@cazcade.com>
2014-08-23 03:27:34 +0100

    Health check

--- providers/do/do_common.sh
+++ providers/do/do_common.sh
@@ -1,4 +1,8 @@
 
-dataCenters=( ["nyc2"]=4 ["nyc1"]=1 ["ams1"]=2 ["sfo1"]=3 ["ams2"]=5 ["sgp1"]=6 ["lon1"]=7 ["nyc3"]=8)
+dataCenters=( "nyc2:4" "nyc1:1" "ams1:2" "sfo1:3" "ams2:5" "sgp1:6" "lon1:7" "nyc3:8")
 
-export DO_REGION=${dataCenters["$DATACENTER"]}
+for dataCenter in "${dataCenters[@]}" ; do
+    KEY="${dataCenter%%:*}"
+    VALUE="${dataCenter##*:}"
+    [[ $KEY == $DATACENTER ]] && export DO_REGION=${VALUE}
+done

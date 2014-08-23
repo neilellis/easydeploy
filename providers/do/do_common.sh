ad1bcfb1252c1ad1391dc5192b8a01348f3becc8
Neil Ellis <neil@cazcade.com>
2014-08-23 03:10:39 +0100

    Health check

--- providers/do/do_common.sh
+++ providers/do/do_common.sh
@@ -1,3 +1,4 @@
+
 dataCenters=( ["nyc2"]=4 ["nyc1"]=1 ["ams1"]=2 ["sfo1"]=3 ["ams2"]=5 ["sgp1"]=6 ["lon1"]=7 ["nyc3"]=8)
 
-export DO_REGION=${dataCenters[$DATACENTER]}
+export DO_REGION=${dataCenters["$DATACENTER"]}

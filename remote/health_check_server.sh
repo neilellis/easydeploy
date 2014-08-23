c0216074c80a1315474a5d9f91f4cc09f779be80
Neil Ellis <neil@cazcade.com>
2014-08-23 01:22:13 +0100

    Health check

--- remote/health_check_server.sh
+++ remote/health_check_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 /ezbin/health_check.sh > /tmp/hco
-while  ! grep FAIL < /tmp/hco
+while  ! tail -1 < /tmp/hco | grep FAIL
 do
-    /bin/nc.traditional -l -p 1888 -c "(/ezbin//health_check.sh > /tmp/hco || cat /tmp/hco) || exit 1"
+    /bin/nc.traditional -l -p 1888 -c "/ezbin/health_check.sh  > /tmp/hco && (/bin/echo -e 'HTTP/1.1 200 OK\r\n\r\n') || (/bin/echo -e 'HTTP/1.1 500 FAIL\r\n\r\n' && exit -1)"
 done

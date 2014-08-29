d2d1100ba58ca451a1c41715cabb9c8603302ddc
Neil Ellis <neil@cazcade.com>
2014-08-29 17:11:13 +0100

    Health check

--- remote/run-docker.sh
+++ remote/run-docker.sh
@@ -49,7 +49,7 @@ fi
 
 docker stop ${COMPONENT}-${OFFSET} || :
 docker rm --force ${COMPONENT}-${OFFSET} || :
-trap "docker stop ${COMPONENT}-${OFFSET} || : ; echo 'SIGTERM' ; exit 0" SIGTERM
+trap "docker stop ${COMPONENT}-${OFFSET} || : ; docker rm --force ${COMPONENT}-${OFFSET} || : ; echo 'SIGTERM' ; exit 0" SIGTERM
 
 docker run --name ${COMPONENT}-${1} --rm=true  --sig-proxy=true -t -i $DOCKER_ARGS -v /var/easydeploy/container/$1:/var/local -v /var/log/easydeploy/container/$1:/var/log/easydeploy -v /var/easydeploy/share:/var/share -v /var/easydeploy/share:/var/easydeploy/share -e EASYDEPLOY_HOST_IP=${EASYDEPLOY_HOST_IP} --dns ${EASYDEPLOY_HOST_IP} ${dockerLinks} ${DOCKER_IMAGE}:${DEPLOY_ENV} ${DOCKER_COMMANDS}
 

f3b043e25c79076358218f9c71f9f033439e1b01
Neil Ellis <neil@cazcade.com>
2014-08-29 17:47:06 +0100

    Health check

--- remote/health_check_server.sh
+++ remote/health_check_server.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -eux
 /ezbin/health_check.sh > /tmp/hco
+
+function check() {
+    if /ezbin/health_check.sh  > /tmp/hco
+    then
+        (/bin/echo -ne 'HTTP/1.1 200 OK\r\n\r\n')
+    else
+        (/bin/echo -ne 'HTTP/1.1 500 FAIL\r\n\r\n' && exit 1)
+    fi
+}
+
+export -f check
+
 while  ! tail -1 < /tmp/hco | grep FAIL
 do
-    /bin/nc.traditional -l -p 1888 -c "/ezbin/health_check.sh  > /tmp/hco && (/bin/echo -ne 'HTTP/1.1 200 OK\r\n\r\n') || (/bin/echo -ne 'HTTP/1.1 500 FAIL\r\n\r\n' && exit -1)"
+    /bin/nc.traditional -l -p 1888 -c "/bin/bash -eux check"
 done

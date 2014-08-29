a997d53a52ac225dc913ad01e4aced09d0a056f8
Neil Ellis <neil@cazcade.com>
2014-08-29 03:58:25 +0100

    Health check

--- commands/rebuild-app.sh
+++ commands/rebuild-app.sh
@@ -11,11 +11,11 @@ then
     for machine in $machines
     do
             sync ${DIR}/*  easydeploy@${machine}:~/project/
-            ssh  -o "StrictHostKeyChecking no" easyadmin@${machine} '/usr/bin/supervisorctl restart $(cat /var/easydeploy/share/.config/component):'
+            ssh  -o "StrictHostKeyChecking no" easyadmin@${machine} 'sudo /usr/bin/supervisorctl restart $(cat /var/easydeploy/share/.config/component):'
             sleep ${1:-30}
     done
 else
-   ../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | parallel --gnu -P 0  "set -eux; sync ${DIR}/*  easydeploy@{}:~/project/;  ssh  -o 'StrictHostKeyChecking no' easyadmin@{} '/usr/bin/supervisorctl restart $(cat /var/easydeploy/share/.config/component):' "
+   ../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | parallel --gnu -P 0  "set -eux; sync ${DIR}/*  easydeploy@{}:~/project/;  ssh  -o 'StrictHostKeyChecking no' easyadmin@{} 'sudo /usr/bin/supervisorctl restart $(cat /var/easydeploy/share/.config/component):' "
 fi
 
 

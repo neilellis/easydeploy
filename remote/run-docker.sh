28570fb08d9c0445e60eeea4b410f9fecf266e76
Neil Ellis <neil@cazcade.com>
2014-08-29 16:37:23 +0100

    Health check

--- remote/run-docker.sh
+++ remote/run-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/easydeploy/bin:/root/bin
 
-OFFSET=$1
+export OFFSET=$1
 set -eu
 
 . /home/easydeploy/bin/env.sh
@@ -46,7 +46,10 @@ if [[ -f Dockerfile ]]
 then
     docker build -t ${DOCKER_IMAGE}:${DEPLOY_ENV}   .
 fi
-docker stop  ${COMPONENT}-${1} || :
+
+trap "docker stop ${COMPONENT}-${OFFSET} || : ; echo 'SIGTERM' ; exit 0" SIGTERM
+
+
 docker run --name ${COMPONENT}-${1} --rm=true --user="easydeploy" --sig-proxy=true -t -i $DOCKER_ARGS -v /var/easydeploy/container/$1:/var/local -v /var/log/easydeploy/container/$1:/var/log/easydeploy -v /var/easydeploy/share:/var/share -v /var/easydeploy/share:/var/easydeploy/share -e EASYDEPLOY_HOST_IP=${EASYDEPLOY_HOST_IP} --dns ${EASYDEPLOY_HOST_IP} ${dockerLinks} ${DOCKER_IMAGE}:${DEPLOY_ENV} ${DOCKER_COMMANDS}
 
 serf tags -set health=failed

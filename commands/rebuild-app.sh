510310d5519dd1dfe54f3ee23f9e29724b4266cf
Neil Ellis <neil@cazcade.com>
2014-09-01 19:43:42 +0100

    MOSH support

--- commands/rebuild-app.sh
+++ commands/rebuild-app.sh
@@ -10,12 +10,12 @@ then
     machines="$(../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | tr '\n' ' ' | tr -s ' ')"
     for machine in $machines
     do
-            sync ${DIR}/*  easydeploy@${machine}:~/project/
+            sync ${DIR}/  easydeploy@${machine}:~/project/
             ssh  -o "StrictHostKeyChecking no" easyadmin@${machine} 'sudo /usr/bin/supervisorctl restart $(cat /var/easydeploy/share/.config/component):'
             sleep ${1:-30}
     done
 else
-   ../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | parallel --gnu -P 0  "set -eux; sync ${DIR}/*  easydeploy@{}:~/project/;  ssh  -o 'StrictHostKeyChecking no' easyadmin@{} 'sudo /usr/bin/supervisorctl restart \$(cat /var/easydeploy/share/.config/component):' "
+   ../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | parallel --gnu -P 0  "set -eux; sync ${DIR}/  easydeploy@{}:~/project/;  ssh  -o 'StrictHostKeyChecking no' easyadmin@{} 'sudo /usr/bin/supervisorctl restart \$(cat /var/easydeploy/share/.config/component):' "
 fi
 
 

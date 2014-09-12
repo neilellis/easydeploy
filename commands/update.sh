65ac8ffe938e636d1d1945ccb8aabe559f9d0bc6
Neil Ellis <neil@cazcade.com>
2014-09-12 20:09:56 +0100

    MOSH support

--- commands/update.sh
+++ commands/update.sh
@@ -9,11 +9,9 @@ then
     for machine in $machines
     do
             ./deploy.sh $machine
-            ssh  -o "StrictHostKeyChecking no" easyadmin@${machine} "sudo reboot"
-            sleep ${1:-60}
     done
 else
-   ../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | parallel --gnu -P 0 --no-run-if-empty  "./deploy.sh {} ; ssh  -o 'StrictHostKeyChecking no' easyadmin@{} 'sudo reboot'"
+   ../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | parallel --gnu -P 0 --no-run-if-empty  "./deploy.sh {}"
 fi
 
 

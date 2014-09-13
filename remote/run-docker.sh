56a4015b038e93e1e25edebb6eb1636a130f19e6
Neil Ellis <neil@cazcade.com>
2014-09-13 08:53:45 +0100

    MOSH support

--- remote/run-docker.sh
+++ remote/run-docker.sh
@@ -12,6 +12,8 @@ done
 [ -d /var/easydeploy/container/$1 ] || mkdir -p /var/easydeploy/container/$1
 [ -d /var/log/easydeploy/container/$1 ] || mkdir -p /var/log/easydeploy/container/$1
 [ -d /var/log/easydeploy/container/$1/data ] || mkdir -p /var/log/easydeploy/container/$1/data
+chown 9999 /var/log/easydeploy/container/$1/data
+
 
 if [ ! -z "$EASYDEPLOY_WAIT_FOR" ]
 then

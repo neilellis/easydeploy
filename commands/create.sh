f623ae03b9ebfd7192bbae10cbd752ea07ea6685
Neil Ellis <neil@cazcade.com>
2014-09-23 14:54:45 +0100

    Clean before rebuild

--- commands/create.sh
+++ commands/create.sh
@@ -4,9 +4,10 @@ cd $(dirname $0) &> /dev/null
 set -eux
 ./update.sh
 ./scale.sh min
+./remote.sh "sudo /ezbin/clean.sh; sudo reboot"
 while ! ./wire.sh
 do
-    ./remote.sh "sudo reboot"
+
     echo "Retrying to wire"
     sleep 60
 done

43ca46cd4ce8323ab360001f4ffa8705a9721aa3
Neil Ellis <neil@cazcade.com>
2014-09-27 00:56:13 +0100

    Weave support

--- providers/do/make-image.sh
+++ providers/do/make-image.sh
@@ -5,24 +5,24 @@ cd $(dirname $0)
 
 trap 'echo error "${BASH_SOURCE}" "${LINENO}" "$?" >&2; echo FAILED' ERR
 
-if $tugboat info_image -n $2
+if $tugboat info_image -n $2  >&2
 then
-    $tugboat destroy_image -c -n $2
+    $tugboat destroy_image -c -n $2  >&2
 fi
 
-$tugboat ssh --ssh-user=${USERNAME} -q -c "sudo poweroff" $1
+$tugboat ssh --ssh-user=${USERNAME} -q -c "sudo poweroff" $1  >&2
 while ! tugboat wait -s off $1
 do
-    echo "Waiting for poweroff."
+    echo "Waiting for poweroff." >&2
     sleep 5
 done
 
 sleep 5
 
-$tugboat snapshot $2 $1
-while ! tugboat wait $1
+$tugboat snapshot $2 $1 >&2
+while ! tugboat wait $1   >&2
 do
-    echo "Waiting for machine to restart."
+    echo "Waiting for machine to restart." >&2
     sleep 5
 done
 $tugboat info_image -n $2 | grep "ID:" | cut -d':' -f2 | tr -d ' '

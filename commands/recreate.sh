db0bbe06b950ec444416b1d090b077b9b00c3b33
Neil Ellis <neil@cazcade.com>
2014-09-27 16:41:11 +0100

    Weave support

--- commands/recreate.sh
+++ commands/recreate.sh
@@ -3,7 +3,7 @@ cd $(dirname $0) &> /dev/null
 . common.sh
 set -eux
 ./destroy.sh
-while (( $(../providers/${PROVIDER}/list-machines.sh | grep "^${MACHINE_NAME}:" | wc -l ) > 0 ))
+while (( $(../providers/${PROVIDER}/list-machines.sh | grep "^$(mc_name):" | wc -l ) > 0 ))
 do
     echo "Waiting for destruction ..."
     sleep 60

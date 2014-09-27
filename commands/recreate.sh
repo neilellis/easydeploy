cf80070340fb0c6edc336f82de9b1d377c52b26c
Neil Ellis <neil@cazcade.com>
2014-09-27 20:29:17 +0100

    Weave support

--- commands/recreate.sh
+++ commands/recreate.sh
@@ -2,7 +2,7 @@
 cd $(dirname $0) &> /dev/null
 . common.sh
 set -eux
-./destroy.sh
+./rebuild-machines.sh
 while (( $(../providers/${PROVIDER}/list-machines.sh | grep "^$(mc_name):" | wc -l ) > 0 ))
 do
     echo "Waiting for destruction ..."

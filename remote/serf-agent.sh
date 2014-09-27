dc0d4db8a43acbf8c842289c00eb9416c63fe925
Neil Ellis <neil@cazcade.com>
2014-09-27 19:49:34 +0100

    Weave support

--- remote/serf-agent.sh
+++ remote/serf-agent.sh
@@ -12,4 +12,4 @@ fi
 [ -d /var/serf ] || mkdir /var/serf
 
 #Assign a node name, bind to the public ip, add relevant tags and the event handlers.
-/usr/local/bin/serf agent -snapshot /var/serf/snapshot -encrypt=$(cat /var/easydeploy/share/.config/serf_key) -node=$(cat /var/easydeploy/share/.config/hostname)-${EASYDEPLOY_HOST_IP} -bind ${EASYDEPLOY_HOST_IP} -tag deploy_env=${DEPLOY_ENV} -tag project=${PROJECT} -tag component=${COMPONENT} -event-handler /etc/serf/event-handler.sh  -event-handler  member-join,member-leave,member-failed,member-update,member-reap=/etc/serf/handlers/membership-handler.sh || (sleep 20 && exit -1)
+/usr/local/bin/serf agent -snapshot /var/serf/snapshot -encrypt $(cat /var/easydeploy/share/.config/serf_key) -node $(cat /var/easydeploy/share/.config/hostname)-${EASYDEPLOY_HOST_IP} -bind ${EASYDEPLOY_HOST_IP} -tag deploy_env=${DEPLOY_ENV} -tag project=${PROJECT} -tag component=${COMPONENT} -event-handler /etc/serf/event-handler.sh  -event-handler  member-join,member-leave,member-failed,member-update,member-reap=/etc/serf/handlers/membership-handler.sh || (sleep 20 && exit -1)

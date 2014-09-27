25842937aa3c26cdf4759f1c2617976ae2da7e49
Neil Ellis <neil@cazcade.com>
2014-09-27 21:30:32 +0100

    Weave support

--- commands/recreate.sh
+++ commands/recreate.sh
@@ -3,9 +3,4 @@ cd $(dirname $0) &> /dev/null
 . common.sh
 set -eux
 ./rebuild-machines.sh
-while (( $(../providers/${PROVIDER}/list-machines.sh | grep "^$(mc_name):" | wc -l ) > 0 ))
-do
-    echo "Waiting for destruction ..."
-    sleep 60
-done
 ./create.sh

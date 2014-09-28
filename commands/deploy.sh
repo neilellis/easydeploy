089ec6d2ed88b50872dc58950eb169412f411552
Neil Ellis <neil@cazcade.com>
2014-09-28 23:02:23 +0100

    Weave support

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -62,7 +62,7 @@ fi
 
 rscp  ~/.ezd/serf_key ${USERNAME}@${IP_ADDRESS}:~/serf_key
 
-docker build -t $(mc_name) .
+docker build -t $(mc_name) ${DIR}
 docker push $(mc_name)
 
 ssh  -qo "StrictHostKeyChecking no" ${USERNAME}@${IP_ADDRESS} "~/remote/bootstrap.sh ${DATACENTER} ${COMPONENT} ${DEPLOY_ENV} ${PROJECT} ${BACKUP_HOST} $(mc_name)  ${LB_TARGET_COMPONENT:-${COMPONENT}} ${REMOTE_IP_RANGE} \"${APP_ARGS}\" "

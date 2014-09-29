2f22fbf4928edba978518ef0c30cbf8d72c0c631
Neil Ellis <neil@cazcade.com>
2014-09-29 06:44:42 +0100

    Weave support

--- commands/create.sh
+++ commands/create.sh
@@ -4,6 +4,8 @@ cd $(dirname $0) &> /dev/null
 set -eux
 #./update.sh
 ./scale.sh min
+docker build -t ${DOCKER_IMAGE}:${DEPLOY_ENV} ${DIR}
+docker push ${DOCKER_IMAGE}:${DEPLOY_ENV}
 ./remote.sh "sudo reboot"
 sleep 30
 while ! ./wire.sh
--- commands/recreate.sh
+++ commands/recreate.sh
@@ -3,7 +3,4 @@ cd $(dirname $0) &> /dev/null
 . common.sh
 set -eux
 ./rebuild-machines.sh
-docker build -t ${DOCKER_IMAGE}:${DEPLOY_ENV} ${DIR}
-docker push ${DOCKER_IMAGE}:${DEPLOY_ENV}
-
 ./create.sh

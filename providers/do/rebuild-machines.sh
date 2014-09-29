647f437363de61798e88fa22001e520ca58b5429
Neil Ellis <neil@cazcade.com>
2014-09-29 06:43:41 +0100

    Weave support

--- providers/do/rebuild-machines.sh
+++ providers/do/rebuild-machines.sh
@@ -18,7 +18,7 @@ function rebuild() {
     do
         $tugboat halt -c -i $1
         $tugboat wait -i $1 -s off
-        if $tugboat rebuild -c -k $2 -i ${DO_BASE_IMAGE}
+        if $tugboat rebuild -c -k ${DO_BASE_IMAGE} -i $1
          then
             break;
          else

b849e75fb2a76f51e1bc1479390d214e0155f8cb
Neil Ellis <neil@cazcade.com>
2014-09-29 14:56:16 +0100

    Weave support

--- commands/update.sh
+++ commands/update.sh
@@ -10,7 +10,7 @@ then
     for machine in $machines
     do
             ./deploy.sh $machine
-            ../commands/bootstrap.sh $@
+            ./bootstrap.sh $@
     done
 else
    ../providers/${PROVIDER}/list-machines-by-ip.sh $(mc_name) | parallel --gnu -P 0 --no-run-if-empty  "./deploy.sh {}"
